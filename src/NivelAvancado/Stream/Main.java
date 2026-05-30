8d0bfe75cd7b8144786f968a690081729ce8013d
Henrique Abreu <abreuh873@gmail.com>
2026-05-30 15:02:48 -0300

    Praticando Streams

--- src/NivelAvancado/Stream/Main.java
+++ src/NivelAvancado/Stream/Main.java
@@ -2,6 +2,8 @@ package NivelAvancado.Stream;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
     static void main(String[] args) {
@@ -11,7 +13,10 @@ public class Main {
         ninjas.add(new Ninja("Hiruzen Sarutobi", "Konoha", 69));
         ninjas.add(new Ninja("Kakashi Hatake", "Konoha", 20));
         ninjas.add(new Ninja("Killer Bee", "Nuvem", 35));
-
+        ninjas.add(new Ninja("Gaara", "Areia", 12));
+        ninjas.add(new Ninja("Temari", "Areia", 15));
+        ninjas.add(new Ninja("Haku", "Nevoa", 15));
+        ninjas.add(new Ninja("Zabuza Momochi", "Nevoa", 26));
         /*ninjas.stream()
                 .filter(ninja -> ninja.getAldeia().equals("Nuvem"))
                 .forEach(System.out::println);*/
@@ -24,12 +29,23 @@ public class Main {
                 .sorted((n1, n2) -> CharSequence.compare(n1.getNome(), n2.getNome()))
                 .forEach(System.out::println);*/
 
-/*        ninjas.stream()
+        /*ninjas.stream()
                 .sorted((Comparator.comparing(Ninja::getNome).reversed()))
                 .forEach(System.out::println);*/
 
         ninjas.stream()
-                .sorted((n1, n2) -> CharSequence.compare(n1.getNome(), n2.getNome()))
+                .filter(ninja -> ninja.getAldeia().equals("Konoha"))
+                .sorted((n1, n2) -> (CharSequence.compare(n1.getNome(), n2.getNome())))
+                .map(Ninja::getNome)
                 .forEach(System.out::println);
+
+        //Teste Final com Streams
+        List<String> ninjasList = ninjas.stream()
+                .filter(ninja -> ninja.getAldeia().equals("Konoha"))
+                .sorted(Comparator.comparing(Ninja::getNome).reversed())
+                .map(Ninja::getNome)
+                .toList();
+
+        System.out.println(ninjasList);
     }
 }
